fa5bf7cb0988080c604c547d7ce45820ec3ff9f4
Nikitos <nikita4651@yandex.ru>
2026-06-22 11:43:09 +0300

    CI success

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -25,7 +25,7 @@ class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
